5bc1b9fae81bf95342409b9f2fd79c5b6dddcc5f
Diogo Cardoso Fernandes <diogofernandes@outlook.com>
2019-04-11 16:55:34 -0300

    adicionado tiro

--- game.py
+++ game.py
@@ -7,12 +7,14 @@ from pygame.locals import *
 SCREENRECT = Rect(0, 0, 640, 640)
 PLAYER_SPEED = 12
 ALIEN_SPEED = 10
+SHOT_SPEED = 10
 
 
 # IMAGENS
 playerImage = pygame.image.load("player.png")
 alienImage = pygame.image.load("alien.png")
 background = pygame.image.load("background.png")
+shotImage = pygame.image.load("bullet.png")
 
 
 class Actor:
@@ -52,6 +54,17 @@ class Alien(Actor):
             self.rect = self.rect.clamp(SCREENRECT)
 
 
+class Shot(Actor):
+
+    def __init__(self, player):
+        Actor.__init__(self, shotImage)
+        self.rect.centerx = player.rect.centerx
+        self.rect.top = player.rect.top - 10
+
+    def update(self):
+        self.rect.top = self.rect.top - SHOT_SPEED
+
+
 pygame.init()
 
 screen = pygame.display.set_mode(SCREENRECT.size)
@@ -59,7 +72,7 @@ screen = pygame.display.set_mode(SCREENRECT.size)
 # CRIA AS SPRITES
 player = Player()
 alien = Alien()
-
+shot = Shot(player)
 # LOOP ETERNO
 while 1:
 
@@ -73,10 +86,17 @@ while 1:
     direction = keystate[K_RIGHT] - keystate[K_LEFT]
     player.move(direction)
 
-    # MOVE O ALIEN
-    alien.update()
+    # CRIA O TIRO
+    if keystate[K_SPACE]:
+        shot = Shot(player)
 
+    # ATUALIZA O RECT DOS OBJETOS
+    alien.update()
+    shot.update()
+
+    # UPDATE NOS RECT PARA ATUALIZAR A TELA
     screen.blit(background, (0, 0))
     screen.blit(alienImage, alien.rect)
     screen.blit(playerImage, player.rect)
+    screen.blit(shotImage, shot.rect)
     pygame.display.flip()
